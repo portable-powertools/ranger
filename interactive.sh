5005cb1c35c416a4d582c5aaffdfec4a2f8f1c5e
Simon Leischnig <simon.leischnig@dlr.de>
2018-07-20 17:23:59 +0200

    newest shortcuts etc

--- interactive.sh
+++ interactive.sh
@@ -1,2 +1,22 @@
 PATH_add "$mod_ranger_sw"
-alias ranger='ranger.py'
+PATH_add "$mod_ranger_root/bin.d"
+export mod_ranger_root
+export mod_ranger_sw
+
+RANGER_LOAD_DEFAULT_RC=FALSE
+
+alias rr=ranger
+alias rcd='ranger-cd'
+
+function ranger-cd {
+    tempfile="$(mktemp -t tmp.XXXXXX)"
+    ranger --choosedir="$tempfile" "${@:-$(pwd)}"
+    test -f "$tempfile" &&
+        if [ "$(cat -- "$tempfile")" != "$(echo -n "$(pwd)")" ]; then
+        cd -- "$(cat "$tempfile")"
+    fi
+    rm -f -- "$tempfile"
+}
+
+# This binds Ctrl-O to ranger-cd:
+bind '"\C-o":"ranger-cd\C-m"'
