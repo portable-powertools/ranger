dd15250c5e258a6ee3ed5b768d25d4f8578ede01
Simon Leischnig <simon.leischnig@dlr.de>
2018-07-20 19:39:43 +0200

    ranger shortcuts for cding

--- interactive.sh
+++ interactive.sh
@@ -8,15 +8,22 @@ RANGER_LOAD_DEFAULT_RC=FALSE
 alias rr=ranger
 alias rcd='ranger-cd'
 
-function ranger-cd {
+export mod_ranger_cdfile="$(mktemp -t rrcd.XXXXXX)"
+log_error "mod_ranger_cdfile: '$mod_ranger_cdfile'"
+
+rrcd() {
+    local dir="$( cat "${mod_ranger_cdfile:-$(pwd)}" )"
+    [[ $dir ]] && cd -- "$dir"
+}
+ranger-cd() {
     tempfile="$(mktemp -t tmp.XXXXXX)"
-    ranger --choosedir="$tempfile" "${@:-$(pwd)}"
+    ranger --choosedir="$tempfile" "$@"
     test -f "$tempfile" &&
         if [ "$(cat -- "$tempfile")" != "$(echo -n "$(pwd)")" ]; then
-        cd -- "$(cat "$tempfile")"
+        cd -- "$( cat "${tempfile:-$(pwd)}" )"
     fi
     rm -f -- "$tempfile"
 }
 
 # This binds Ctrl-O to ranger-cd:
-bind '"\C-o":"ranger-cd\C-m"'
+# bind '"\C-o":"ranger-cd\C-m"'
